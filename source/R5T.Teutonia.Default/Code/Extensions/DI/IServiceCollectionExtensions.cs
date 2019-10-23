ceed14e6be7179111f5330f890e9c97c0d4bd49f
MinexAutomation <minex.automation@gmail.com>
2019-10-23 14:44:31 -0700

    Added DI-container extension to add required services to use the default file-system cloning operator.

---
+++ source/R5T.Teutonia.Default/Code/Extensions/DI/IServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Lombardy;
+
+
+namespace R5T.Teutonia.Default.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Also requies a:
+        /// * <see cref="IStringlyTypedPathOperator"/>
+        /// </summary>
+        public static IServiceCollection UseDefaultFileSystemCloningOperator(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<IFileSystemCloningOperator, DefaultFileSystemCloningOperator>()
+                .AddSingleton<IFileSystemCloningDifferencer, DefaultFileSystemCloningDifferencer>()
+                ;
+
+            return services;
+        }
+    }
+}
