fe0eca34d0edfff26536c6108e74e4d4656b249a
MinexAutomation <minex.automation@gmail.com>
2020-02-18 22:35:01 -0800

    Updated IServiceCollection extensions to use ServiceAction<T> functionality.

--- source/R5T.Teutonia.Default/Code/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Teutonia.Default/Code/Extensions/IServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
+using R5T.Dacia;
 using R5T.Lombardy;
 
 
@@ -10,17 +11,51 @@ namespace R5T.Teutonia.Default.Extensions
     public static class IServiceCollectionExtensions
     {
         /// <summary>
-        /// Also requies a:
-        /// * <see cref="IStringlyTypedPathOperator"/>
+        /// Adds the <see cref="DefaultFileSystemCloningDifferencer"/> implementation of <see cref="IFileSystemCloningDifferencer"/> as a <see cref="ServiceLifetime.Singleton"/>.
         /// </summary>
-        public static IServiceCollection UseDefaultFileSystemCloningOperator(this IServiceCollection services)
+        public static IServiceCollection AddDefaultFileSystemCloningDifferencer(this IServiceCollection services)
+        {
+            services.AddSingleton<IFileSystemCloningDifferencer, DefaultFileSystemCloningDifferencer>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="DefaultFileSystemCloningDifferencer"/> implementation of <see cref="IFileSystemCloningDifferencer"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IFileSystemCloningDifferencer> AddDefaultFileSystemCloningDifferencerAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IFileSystemCloningDifferencer>(() => services.AddDefaultFileSystemCloningDifferencer());
+            return serviceAction;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="DefaultFileSystemCloningOperator"/> implementation of <see cref="IFileSystemCloningOperator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultFileSystemCloningOperator(this IServiceCollection services,
+            ServiceAction<IFileSystemCloningDifferencer> addFileSystemCloningDifferencer,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
         {
             services
                 .AddSingleton<IFileSystemCloningOperator, DefaultFileSystemCloningOperator>()
-                .AddSingleton<IFileSystemCloningDifferencer, DefaultFileSystemCloningDifferencer>()
+                .RunServiceAction(addFileSystemCloningDifferencer)
+                .RunServiceAction(addStringlyTypedPathOperator)
                 ;
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="DefaultFileSystemCloningOperator"/> implementation of <see cref="IFileSystemCloningOperator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IFileSystemCloningOperator> AddDefaultFileSystemCloningOperatorAction(this IServiceCollection services,
+            ServiceAction<IFileSystemCloningDifferencer> addFileSystemCloningDifferencer,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            var serviceAction = new ServiceAction<IFileSystemCloningOperator>(() => services.AddDefaultFileSystemCloningOperator(
+                addFileSystemCloningDifferencer,
+                addStringlyTypedPathOperator));
+            return serviceAction;
         }
     }
 }
